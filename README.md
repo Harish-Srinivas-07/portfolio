64de447bc8909f0f8b541949d999c626267f4154
Harish-Sunteknology <harish.srinivas@sunteknolozy.org>
2025-06-09 16:43:10 +0530

    updated prj

---
+++ README.md
@@ -0,0 +1,7 @@
+# 💼 Harish Srinivas SR - Portfolio Website
+
+Welcome to my personal portfolio website! This site showcases my projects, skills, and professional background. It's a hub where you can explore my work across web, mobile, and full-stack development.
+
+## 🌐 Live Website
+
+🔗 [srharishsrinivas.netlify.app](https://srharishsrinivas.netlify.app)
--- assets/js/script.js
+++ assets/js/script.js
@@ -42,6 +42,26 @@ for (let i = 0; i < testimonialsItem.length; i++) {
     modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
     modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
     modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+    const modalGitLink = modalContainer.querySelector('[data-git-link]');
+const modalDemoLink = modalContainer.querySelector('[data-demo-link]');
+
+const gitLink = this.querySelector('[data-git-link]');
+const demoLink = this.querySelector('[data-demo-link]');
+
+if (gitLink) {
+  modalGitLink.href = gitLink.href;
+  modalGitLink.style.display = 'inline-flex';
+} else {
+  modalGitLink.style.display = 'none';
+}
+
+if (demoLink) {
+  modalDemoLink.href = demoLink.href;
+  modalDemoLink.style.display = 'inline-flex';
+} else {
+  modalDemoLink.style.display = 'none';
+}
+
 
     testimonialsModalFunc();
 
